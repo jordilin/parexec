b83024b172d1d3c3d5aeb68775d8c163daa79e6f
Jordi Carrillo <jordilin@gmail.com>
2020-04-20 22:59:57 -0700

    Add header copyright in main file
    Signed-off-by: Jordi Carrillo <jordilin@gmail.com>

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// Copyright 2020 Jordi Carrillo. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// parexec executes shell commands sequentially and in parallel given a yaml
+// resource description.
+
 package main
 
 import (
